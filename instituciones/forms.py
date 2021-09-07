a1ccb26c46a9e73b7b3b288354f56eee852a5e63
dgomezc10 <dgomezc10@eafit.educ.co>
2021-09-06 21:52:21 -0500

    Correcionde vista (html) registro de docente

--- instituciones/forms.py
+++ instituciones/forms.py
@@ -1,14 +1,27 @@
 from django import forms
 from django.forms import widgets
 from .models import Institucion
+import re
 
 class FormularioInstitucion(forms.ModelForm):
     class Meta:
         model =  Institucion
         fields = ('nombre', 'localidad', 'barrio', 'sede')
         widgets = {
-            'nombre': forms.TextInput(attrs={'placeholder': 'Nombre'}),
-            'localidad': forms.TextInput(attrs={'placeholder': 'Municipio'}),
-            'barrio': forms.TextInput(attrs={'placeholder': 'Barrio'}),
-            'sede': forms.TextInput(attrs={'placeholder': 'Sede', 'required':False}),
+            'nombre': forms.TextInput(attrs={'placeholder': 'Ingrese el nombre...'}),
+            'localidad': forms.TextInput(attrs={'placeholder': 'Ingrese el municipio...'}),
+            'barrio': forms.TextInput(attrs={'placeholder': 'Ingrese el barrio...'}),
+            'sede': forms.TextInput(attrs={'placeholder': 'Ingrese la sede...', 'required':False}),
         }
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if bool(re.search(r'\d', nombre)):
+            raise forms.ValidationError('El nombre contiene caracteres invalidos')
+        return nombre
+
+    def clean_localidad(self):
+        localidad = self.cleaned_data.get('localidad')
+        if bool(re.search(r'\d', localidad)):
+            raise forms.ValidationError('El nombre contiene caracteres invalidos')
+        return localidad
