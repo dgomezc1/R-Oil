8ec3333340125cd87efaf1d41235ac25149b80c2
dgomezc10 <dgomezc10@eafit.educ.co>
2021-09-08 17:12:47 -0500

    Conversion de view registro de aceite a view based class

--- aceite/urls.py
+++ aceite/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('registro/', views.registro_de_aceite, name='registro de aceite'),
+    #path('registro/', views.registro_de_aceite, name='registro de aceite'),
+    path('registro/', views.registro_de_aceite2.as_view(), name='registro de aceite'),
 ]
--- aceite/views.py
+++ aceite/views.py
@@ -9,7 +9,8 @@ from usuario.models import User
 from estudiante.models import Estudiante
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import registro_aceite
-
+from django.views import View
+from usuario.mixins import permisos_institucion_docentes
 # Create your views here.
 
 
@@ -28,3 +29,23 @@ def registro_de_aceite(request):
         else:
             return render(request,'registro_Aceite.html' ,{'form':form})
     return render(request,'registro_Aceite.html' ,{'form':form})
+
+class registro_de_aceite2(permisos_institucion_docentes, View):
+    form_class  = FormularioAceite()
+    template_name = 'registro_Aceite.html'
+
+    def get(self, request, *args, **kwargs):
+        form = FormularioAceite()
+        return render(request, self.template_name, {'form':form})
+    
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        form = FormularioAceite(request.POST)
+        if form.is_valid():
+            nombre= form.cleaned_data['username']
+            estudiante = User.objects.get(username = nombre)
+            print(estudiante)
+            estudiante1=Estudiante.objects.get(user=estudiante)
+            form.crear_registro(estudiante1)
+            return HttpResponse("Envio correcto de datos")
+
+        return render(request, self.template_name, {'form':form})
