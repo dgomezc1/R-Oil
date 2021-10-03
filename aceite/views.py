2801285e6a497212567f9f51df0a3f4cd1d29694
dgomezc10 <dgomezc10@eafit.educ.co>
2021-10-02 23:31:07 -0500

    Correcciones paquete Aceite. Cambios en index.html y style.css (cambios en la interfaz de usuario)

--- aceite/views.py
+++ aceite/views.py
@@ -13,23 +13,6 @@ from django.views import View
 from usuario.mixins import permisos_estudiante_aceite
 # Create your views here.
 
-
-
-def registro_de_aceite(request):
-    form = FormularioAceite()
-    if request.method=='POST':
-        form = FormularioAceite(request.POST)
-        if form.is_valid():
-            nombre= form.cleaned_data['username']
-            estudiante = User.objects.get(username = nombre)
-            print(estudiante)
-            estudiante1=Estudiante.objects.get(user=estudiante)
-            form.crear_registro(estudiante1)
-            return HttpResponse("El registro de aceite se hizo de manera correcta")
-        else:
-            return render(request,'registro_Aceite.html' ,{'form':form})
-    return render(request,'registro_Aceite.html' ,{'form':form})
-
 class registro_de_aceite2(permisos_estudiante_aceite, View):
     form_class  = FormularioAceite()
     template_name = 'registro_Aceite.html'
