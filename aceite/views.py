26cafbdeadaa6597815fa9c0cfae15be0f8e5c11
dgomezc10 <dgomezc10@eafit.educ.co>
2021-10-09 21:19:22 -0500

    Adicion de verficiacion en registro de acite (instucion del docente igual a al instuticion del estudiante)

--- aceite/views.py
+++ aceite/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, HttpResponse
 from .forms import FormularioAceite
 from usuario.models import User
 from estudiante.models import Estudiante
+from docente.models import Docente
+from gestores.models import Gestores
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from usuario.mixins import permisos_estudiante_aceite
@@ -21,10 +23,20 @@ class registro_de_aceite2(LoginRequiredMixin, permisos_estudiante_aceite, View):
         if form.is_valid():
             nombre= form.cleaned_data['username']
             estudiante = User.objects.get(username = nombre)
-            print(estudiante)
             estudiante1=Estudiante.objects.get(user=estudiante)
             institucion = estudiante1.institucion
-            form.crear_registro(estudiante1, institucion)
-            return HttpResponse("Envio correcto de datos")
+            institucionD = None
+            if request.user.admin_docente:
+                docente = User.objects.get(username = request.user)
+                institucionD  =  (Docente.objects.get(user = docente)).institucion
+            else:
+                docente = User.objects.get(username = request.user)
+                institucionD  =  (Gestores.objects.get(user = docente)).institucion
+            if institucionD != institucion:
+                error = "El estudiante no corresponde a su institucion"
+                return render(request, self.template_name, {'form':form, 'error': error, 'act': True})
+            else:
+                form.crear_registro(estudiante1, institucion)
+                return HttpResponse("Envio correcto de datos")
 
         return render(request, self.template_name, {'form':form})
